c7a1be66eff1e709927882db4cd6f206daf90240
Isaiasgomz <isaiasgomz027@gmail.com>
2022-09-21 10:28:09 -0500

    first committ

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int speed = 10;
+        System.out.println(speed);
+        int salary = 1000;
+        String employeeName = "Isaias gomez";
+        System.out.println(employeeName);
+        System.out.println(salary);
+    }
+}
